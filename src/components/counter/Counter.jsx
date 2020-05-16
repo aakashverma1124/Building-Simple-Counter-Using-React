53761c17a8519f94110c187a3a26d019a67db31c
aakashverma1124 <info.aakash11@gmail.com>
2020-05-16 17:44:06 +0530

    removing functionality from child Component

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -18,6 +18,9 @@ class Counter extends Component {
     render() {
         return (
             <div className="App">
+                <div>
+                    <h1>A Simple Counter</h1>
+                </div>
                 <CounterButton by={1} incrementMethod={this.increment} decrementMethod={this.decrement}/>
                 <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement}/>
                 <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement}/>
@@ -38,7 +41,6 @@ class Counter extends Component {
     }
 
     increment(by) { // update state
-        //console.log(`increment from parent: ${by}`)
         this.setState(
             (prevState) => {
                 return {counter: prevState.counter + by}
@@ -47,55 +49,28 @@ class Counter extends Component {
     }
 
     decrement(by) { // update state
-        //console.log(`increment from parent: ${by}`)
         this.setState(
             (prevState) => {
                 return {counter: prevState.counter - by}
             }
         );
     }
-
 }
 
 
 class CounterButton extends Component {
-    constructor() {
-        super();
-        this.state = {
-            counter : 0
-        }
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-    }
 
-    // render = () = {      // in that case we do not need to bind the function as we have bounded it above.
+    // render = () => {      // in that case we do not need to bind the function as we have bounded it above.
     render() {
-
         // const style = {fontSize: "50px", padding: "15px 30px"};
         // We could have done something like: style = {style} in span as an attribute that means passing css inline.
         return (
             <div className="counter">
-                <button onClick={this.increment}>+{this.props.by}</button>
-                <button onClick={this.decrement}>-{this.props.by}</button>
+                <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+                <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
             </div>
         )
     }
-
-    increment() {
-        this.setState({
-            counter: this.state.counter + this.props.by
-        });
-        this.props.incrementMethod(this.props.by)
-    }
-
-    decrement() {
-        this.setState({
-            counter: this.state.counter - this.props.by
-        });
-        this.props.decrementMethod(this.props.by)
-    }
-
-
 }
 
 CounterButton.defaultProps = {
