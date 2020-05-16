d24f2ea4e5f2d632f90dd2d255a0c9b43d76ec3a
aakashverma1124 <info.aakash11@gmail.com>
2020-05-16 17:38:44 +0530

    added CounterButtons and functionality

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <Counter></Counter>
+          <Counter />
+
         </div>
     );
   }
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,8 +3,6 @@ import './Counter.css'
 
 class Counter extends Component {
 
-    // the best practice is to define a initial state in a constructor
-
     constructor() {
         super();
         this.state = {
@@ -13,24 +11,95 @@ class Counter extends Component {
         // The this object of the bound function is associated with the specified object,
         // and has the specified initial parameters
         this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
     render() {
         return (
-            <div className="counter">
-                <button onClick={this.increment}>+1</button>
+            <div className="App">
+                <CounterButton by={1} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+                <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement}/>
+                <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement}/>
                 <span className="count">{this.state.counter}</span>
+                <div>
+                    <button className="reset" onClick={this.reset}>Reset</button>
+                </div>
             </div>
-        )
+        );
     }
 
-    increment() { // update state
-        this.setState({
-            counter: this.state.counter + 1
-        });
+    reset() {
+        this.setState(
+            (prevState) => {
+                return {counter: 0}
+            }
+        );
+    }
+
+    increment(by) { // update state
+        //console.log(`increment from parent: ${by}`)
+        this.setState(
+            (prevState) => {
+                return {counter: prevState.counter + by}
+            }
+        );
+    }
+
+    decrement(by) { // update state
+        //console.log(`increment from parent: ${by}`)
+        this.setState(
+            (prevState) => {
+                return {counter: prevState.counter - by}
+            }
+        );
     }
 
 }
 
+
+class CounterButton extends Component {
+    constructor() {
+        super();
+        this.state = {
+            counter : 0
+        }
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+
+    // render = () = {      // in that case we do not need to bind the function as we have bounded it above.
+    render() {
+
+        // const style = {fontSize: "50px", padding: "15px 30px"};
+        // We could have done something like: style = {style} in span as an attribute that means passing css inline.
+        return (
+            <div className="counter">
+                <button onClick={this.increment}>+{this.props.by}</button>
+                <button onClick={this.decrement}>-{this.props.by}</button>
+            </div>
+        )
+    }
+
+    increment() {
+        this.setState({
+            counter: this.state.counter + this.props.by
+        });
+        this.props.incrementMethod(this.props.by)
+    }
+
+    decrement() {
+        this.setState({
+            counter: this.state.counter - this.props.by
+        });
+        this.props.decrementMethod(this.props.by)
+    }
+
+
+}
+
+CounterButton.defaultProps = {
+    by : 1
+}
 
 export default Counter
